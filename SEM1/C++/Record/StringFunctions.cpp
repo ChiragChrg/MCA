c2eecc866b4653591ae53dfd7b7f1bdc6681fd50
ChiragChrg <chiruchirag2001@gmail.com>
2024-05-02 00:54:36 +0530

    Optimized String compare function

--- SEM1/C++/Record/StringFunctions.cpp
+++ SEM1/C++/Record/StringFunctions.cpp
@@ -99,8 +99,10 @@ void reverse(char str[10]){
 void compare(char str1[10], char str2[10]){
     int i, flag = 0;
     for(i=0; str1[i]!='\0'; i++){
-        if(str1[i] != str2[i])
+        if(str1[i] != str2[i]){
             flag = 1;
+            break;
+        }
     }
 
     if(flag == 0)
